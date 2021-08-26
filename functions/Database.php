f6ba4e3988fedb76c1cc2f6a667c3ad2e6e35799
kurt john <kjmojado21@gmail.com>
2021-08-26 19:31:45 +0800

    fixed database and index

--- functions/Database.php
+++ functions/Database.php
@@ -2,10 +2,16 @@
 session_start();
 
 function db_connect(){
-    $servername = 'localhost';
-    $username = 'root';
-    $password = ''; // if youre using mac, password is root. if youre using windows no passsword 
-    $db_name = 'aug-crud';
+    // $servername = 'localhost';
+    // $username = 'root';
+    // $password = ''; // if youre using mac, password is root. if youre using windows no passsword 
+    // $db_name = 'aug-crud';
+
+    $servername = 'us-cdbr-east-04.cleardb.com';
+    $username = 'bfc3862864e74d';
+    $password = '224302c4'; // if youre using mac, password is root. if youre using windows no passsword 
+    $db_name = 'heroku_b922f80f5178bc0';
+
 
     $conn =  new mysqli($servername,$username,$password,$db_name);
 
